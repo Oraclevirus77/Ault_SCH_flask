98c009eee25cbd6d1806e0a77dc86e3904407826
olakayCoder1 <programmerolakay@gmail.com>
2022-10-10 22:25:57 +0100

    fix todos create list endpoint

--- app.py
+++ app.py
@@ -23,27 +23,30 @@ def hello_world():  # put application code here
     return 'Hello World'
 
 
-@app.route('/todos', methods=['POST'])
+@app.route('/todos', methods=['GET', 'POST'])
 def new_todos():
     global total_todos
     global todos_list
+    if request.method == 'GET':
+        return Response( json.dumps(todos_list), status=200, mimetype='application/json' )
+
+
     request_data = request.get_json()
     todo = parse_todo_request(request_data)
     if None in todo.values():
-        response = Response = Response(
-            "Invalid request",
+        return Response(
+            'Invalid request',
             status=400,
             mimetype='application/json'
         )
-        return response
-    save_todo(todo, todos_list)
-    total_todos += 1
-    response: Response = Response(
-        json.dumps(todo),
-        satus=201,
-        mimetype='application/json')
-    return response
+    save_todo(todo , todos_list)
+    total_todos += 1 
+    return Response(
+        json.dumps(todo), 
+        status=201,
+        mimetype='application/json'
+    )
 
 
 if __name__ == '__main__':
-    app.run
+    app.run(debug=True)
